2cd03acb78eb8ac3e49ef437ecefd9905dc66a9b
Ledovec <ledovec.official@gmail.com>
2023-07-08 13:24:20 +0200

    config impl

--- velocity/src/main/java/me/ledovec/auth/velocityauth/VelocityAuth.java
+++ velocity/src/main/java/me/ledovec/auth/velocityauth/VelocityAuth.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 import me.ledovec.auth.velocityauth.events.PlayerListener;
 import me.ledovec.auth.velocityauth.commands.LoginCommand;
@@ -12,9 +13,16 @@ import me.ledovec.auth.velocityauth.session.SessionFactory;
 import me.ledovec.auth.velocityauth.session.SessionSecurity;
 import me.zort.sqllib.SQLConnectionBuilder;
 import me.zort.sqllib.pool.SQLConnectionPool;
+import org.simpleyaml.configuration.file.FileConfiguration;
+import org.simpleyaml.configuration.file.YamlConfiguration;
 import org.slf4j.Logger;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Plugin(
         id = "velocity_auth",
@@ -24,32 +32,54 @@ import java.io.File;
 public class VelocityAuth {
 
     @Inject private Logger logger;
+
     @Inject private ProxyServer proxyServer;
-    private File configFile;
+
+    private final Path dataDirectory;
+
+    @Inject
+    public VelocityAuth(@DataDirectory Path dataDirectory) {
+        this.dataDirectory = dataDirectory;
+    }
+
+    public static FileConfiguration CONFIGURATION;
 
     public static SQLConnectionPool connectionPool;
 
+    public static List<String> authServers;
+
+    public static List<String> lobbyServers;
+
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
-        configFile = new File("config.yml");
+
+        try {
+            CONFIGURATION = YamlConfiguration.loadConfiguration(new File(dataDirectory + "/config.yml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        authServers = CONFIGURATION.getStringList("auth_servers");
+        lobbyServers = CONFIGURATION.getStringList("lobby_servers");
 
         SQLConnectionPool.Options options = new SQLConnectionPool.Options();
         options.setMaxConnections(1000);
         options.setBorrowObjectTimeout(5000L);
         options.setBlockWhenExhausted(true);
 
-        connectionPool = SQLConnectionBuilder.of("localhost", 3306, "velocity_auth", "root", "")
-                        .createPool(options);
+        connectionPool = SQLConnectionBuilder.of(
+                CONFIGURATION.getString("mysql.address"),
+                CONFIGURATION.getInt("mysql.port"),
+                CONFIGURATION.getString("mysql.database"),
+                CONFIGURATION.getString("mysql.username"),
+                CONFIGURATION.getString("mysql.password"))
+                .createPool(options);
 
         SessionFactory.getInstance().clearAllSessions();
         new SessionSecurity(proxyServer, this).begin();
         proxyServer.getEventManager().register(this, new PlayerListener());
         proxyServer.getCommandManager().register("register", new RegisterCommand(proxyServer), "reg");
         proxyServer.getCommandManager().register("login", new LoginCommand(proxyServer), "log");
-    }
-
-    public File getConfigFile() {
-        return configFile;
     }
 
     public ProxyServer getProxyServer() {
